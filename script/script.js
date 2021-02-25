290ebdb1ef8e691e42f8a2417a49b9d6316d768b
Silena Dangond <silenadangond1@gmail.com>
2021-02-25 17:53:41 -0500

    Actualizaciones del proyecto GIFOS, inicie con el estilo dark

--- script/script.js
+++ script/script.js
@@ -26,3 +26,31 @@ crossMagnifying.addEventListener("click", () => {
     document.querySelector("#magnifying").classList.remove("magnifyingOff")
 })
 
+// GIFOS in Trend 
+const API_KEY = 'VfAQnZwBHdFaj75QChwBF5O4uEoTxQXh'
+
+fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=3`)
+    .then (response => response.json())
+    .then (response => {
+        console.log(response)
+        response.data.forEach( gif => {
+            const image = document.createElement("img")
+            image.src = gif.images.original.url
+            document.querySelector("#newGifos").appendChild(image)
+        })
+    })
+
+// Dark Mode
+const darkMode = document.querySelector("#darkMode")
+darkMode.addEventListener("click", () => {
+    document.body.classList.toggle("dark")
+    //document.querySelector("#header").classList.add("border")
+    darkMode.classList.add(".logoOff")
+    document.querySelector("#firtSection")
+    document.classList.add(".color_white")
+    document.querySelector("#firtSection")
+    document.classList.remove(".color_purple")
+})
+
+
+
