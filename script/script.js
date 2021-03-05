169785f24fdc58ecb03166dd204bf044c1c06ec6
Silena Dangond <silenadangond1@gmail.com>
2021-03-04 20:40:30 -0500

    Avance en Dark Mode y continuo trabajando JavaScript

--- script/script.js
+++ script/script.js
@@ -26,31 +26,8 @@ crossMagnifying.addEventListener("click", () => {
     document.querySelector("#magnifying").classList.remove("off")
 })
 
-// GIFOS in Trend 
+//trending
 const API_KEY = 'VfAQnZwBHdFaj75QChwBF5O4uEoTxQXh'
-
-fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=3`)
-    .then (response => response.json())
-    .then (response => {
-        console.log(response)
-        response.data.forEach( gif => {
-            const image = document.createElement("img")
-            image.src = gif.images.original.url
-            document.querySelector("#newGifos").appendChild(image)
-        })
-    })
-
-// Dark Mode
-const darkMode = document.querySelector("#darkMode")
-darkMode.addEventListener("click", () => {
-    document.body.classList.toggle("dark")
-    document.querySelector("#firtSection")
-    document.classList.remove(".color_purple")
-    document.querySelector("#firtSection")
-    document.classList.add(".color_white")
-})
-
-//trendin
 fetch(`https://api.giphy.com/v1/trending/searches?api_key=${API_KEY}`)
     .then (response => response.json())
     .then (response => {
@@ -64,5 +41,67 @@ fetch(`https://api.giphy.com/v1/trending/searches?api_key=${API_KEY}`)
         document.querySelector(".ultrendings").appendChild(ulTerms) 
     })
 
+// GIFOS in Trend 
+fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=3`)
+    .then (response => response.json())
+    .then (response => {
+        console.log(response)
+        response.data.forEach( gif => {
+            const image = document.createElement("img")
+            image.src = gif.images.original.url
+            document.querySelector("#newGifos").appendChild(image)
+        })
+    })
 
+/*----------------------------*/
+/*         DARK MODE          */
+/*----------------------------*/
+const darkMode = document.querySelector("#darkMode")
+darkMode.addEventListener("click", () => {
+    document.body.classList.toggle("dark")
+    document.querySelector("#menu").classList.add("off")
+    document.querySelector("#cross").classList.add("off")
+    document.querySelector("#burgerDark").classList.remove("off")
+    document.querySelector("#magniDark").classList.remove("off")
+})
+//click - navigation bar
+let crossDark = document.getElementById("burgerDark")
+crossDark.addEventListener("click", () => {
+    crossDark.classList.add("off")
+    document.querySelector("#crossDark").classList.remove("off")
+    document.querySelector("#menu").classList.remove("off")
+    document.querySelector("#darkMode").classList.add("off")
+    document.querySelector("#dayMode").classList.remove("off")
+    document.querySelector("#magniDark").classList.add("off")
+})
 
+let hamburDark = document.getElementById("crossDark")
+hamburDark.addEventListener("click", () => {
+    hamburDark.classList.add("off")
+    document.querySelector("#burgerDark").classList.remove("off")
+    document.querySelector("#menu").classList.add("off")
+    document.querySelector("#magniDark").classList.remove("off")
+})
+//click - GIFOS Finder - First Section
+let magnifDark = document.getElementById("magniDark")
+magnifDark.addEventListener("click", () => {
+    magnifDark.classList.add("off")
+    document.querySelector("#crossMagniDark").classList.remove("off")
+})
+
+let crossMagnifDark = document.getElementById("crossMagniDark")
+crossMagnifDark.addEventListener("click", () => {
+    crossMagnifDark.classList.add("off")
+    document.querySelector("#magniDark").classList.remove("off")
+})
+
+//back dayMode
+const dayMode = document.querySelector("#dayMode")
+dayMode.addEventListener("click", () => {
+    document.body.classList.toggle("dark")
+    document.querySelector("#menu").classList.add("off")
+    document.querySelector("#cross").classList.add("off")
+    document.querySelector("#burger").classList.remove("off")
+    document.querySelector("#burgerDark").classList.add("off")
+    document.querySelector("#crossDark").classList.add("off")
+})
