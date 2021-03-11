53322c2103c5173af9aa4d74b96fc38cfeea1c68
Silena Dangond <silenadangond1@gmail.com>
2021-03-11 17:09:15 -0500

    agregue bonotes para modo dark y agregue muchas otras notas, aun no he podido seleccionar un gifo y mostrarlo en maximo tamaño, continuo trabajando

--- script/script.js
+++ script/script.js
@@ -46,9 +46,11 @@ fetch(`https://api.giphy.com/v1/trending/searches?api_key=${API_KEY}`)
             const liTerms = document.createElement("li")//este li estaba fuera del forEach lo que mostraba
             //en pantalla era el último string traido desde el array, ahora dentro del for, se crearan suficientes
             //li para almacenar cada string del objeto
-            liTerms.textContent = term + ", " 
+            const aTerms = document.createElement("a")
+            aTerms.textContent = term + ", " 
             console.log(term)
             ulcreate.appendChild(liTerms)
+            liTerms.appendChild(aTerms)
             //document.querySelector(".ultrendings").appendChild(ulcreate) 
         })
         //ulcreate.appendChild(liTerms)
@@ -139,12 +141,16 @@ const formGifosFinder = document.querySelector("#gifosFinder")
 const inputTextToSearch = document.querySelector("#searchGifos")
 const lineHr = document.querySelector(".lineUnderSearch")
 const wordSearchH2 = document.querySelector(".termToSearch")
+const tryAnotherSearch = document.querySelector("#tryAnotherSearch")
 const buttonSeeMore = document.querySelector(".seeMore")
 const offH1 = document.querySelector(".color_purple")
 const offIlustration = document.querySelector(".ilustra_header")
-const changePositionCross = document.querySelector("#crossMagnifying")
+const changePositionCross = document.querySelectorAll(".search")
 
 const clickMagnifying = document.querySelector("#magnifying")
+const clickMagniDark = document.querySelector("#magniDark")
+//necesito que la siguiente funcion se ejecute al recibir un click
+//tanto en #magnifying como en #magniDark, debo solucionarlo
 clickMagnifying.addEventListener("click", () =>{
     console.log(inputTextToSearch)
     console.log(inputTextToSearch.value)
@@ -156,6 +162,31 @@ clickMagnifying.addEventListener("click", () =>{
         offH1Title()
         offIlustra_header()
         positionCross()
+    } else {
+        getGifos(inputTextToSearch.value)
+        showTermH2()
+        showLineHr()
+        showIconAnotherSearch()
+    }
+})
+//esta es la funcion para clickMagniDark, aunque parece no funcionar
+clickMagniDark.addEventListener("click", () =>{
+    console.log(inputTextToSearch)
+    console.log(inputTextToSearch.value)
+    if (inputTextToSearch.value !== "") {
+        getGifos(inputTextToSearch.value)
+        showTermH2()
+        showLineHr()
+        showButton()
+        offH1Title()
+        offIlustra_header()
+        positionCross()
+        hiddenIconAnotherSearch()
+    } else {
+        getGifos(inputTextToSearch.value)
+        showTermH2()
+        showLineHr()
+        showIconAnotherSearch()
     }
 })
 
@@ -167,6 +198,12 @@ formGifosFinder.addEventListener("submit" , (text) => {
         showTermH2()
         showLineHr()
         showButton()
+        hiddenIconAnotherSearch()
+    } else {
+        getGifos(inputTextToSearch.value)
+        showTermH2()
+        showLineHr()
+        showIconAnotherSearch()
     }
 })
 
@@ -204,6 +241,15 @@ const showTermH2 = () => {
     wordSearchH2.classList.remove("off")
 }
 
+const showIconAnotherSearch = () => {
+    tryAnotherSearch.classList.remove("off")
+}
+
+const hiddenIconAnotherSearch = () => {
+    tryAnotherSearch.classList.add("off")
+    //buttonSeeMore.classList.add("off")
+}
+
 const showButton = () => {
     buttonSeeMore.classList.remove("off")
 }
@@ -217,15 +263,54 @@ const offIlustra_header = () => {
     offIlustration.classList.add("off")
 }
 
+// function myFunction() {
+//     var x, i;
+//     x = document.querySelectorAll(".example");
+//     for (i = 0; i < x.length; i++) {
+//       x[i].style.backgroundColor = "red";
+//     }ejemplo de stackoverflow
+// }
+
 const positionCross = () => {
     console.log("entro a la función")
-    changePositionCross.classList.replace("search", "searchToTerm")
+    //changePositionCross.classList.replace("search", "searchToTerm")
+    for (i = 0; i < changePositionCross.length; i++) {
+        changePositionCross[i].classList.replace("search", "searchToTerm")
+    }//pude utilizar este for para mejorar la función positionCross 
+    //y la variable changePositionCross accede a todos los nodos con la 
+    //clase .search, gracias al uso de document.querySelectorAll
+    //necesito un for para aplicar el remplazo de clases a todos los elementos
+    //con .search porque all solo aplicara cambios a un solo nodo que yo 
+    //especifique, con el for puedo recorer la longitud de nodos con .search
     console.log(changePositionCross)
 }
 
 //click on trendings words
-const clickLi = document.querySelector(".ultrendings > li")
-clickLi.addEventListener("click", () => {
-    inputTextToSearch.value = li.textContent
-    getGifos(inputTextToSearch.value)
-})
+// const clickLi = document.querySelector(".ultrendings > li > a")
+// clickLi.addEventListener("click", () => {
+//     inputTextToSearch.value = a.textContent
+//     console.log(inputTextToSearch.value)
+//     getGifos(inputTextToSearch.value)
+// })//no funciona esta funcion, continuare trabajando
+
+
+/*----------------------------*/
+/*     MOBILE GIFOS MAX       */
+/*----------------------------*/
+// const clickTrendingGifo = async (gifosTrending) => {
+//     const threeGifos = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=3`)
+//     const data = await response.json() 
+//     console.log(data) 
+//     response.data.forEach( gif => {
+//         const image = document.createElement("img")
+//         image.src = gif.images.original.url
+//         document.querySelector("#newGifos").appendChild(image)
+//     })
+//     let imageNewGifos = document.querySelector("#newGifos > img")
+//     imageNewGifos.addEventListener("click", () => {
+//         const imgUser = document.createElement("h3")
+//         imgUser.textContent = data[i].user.username
+//         console.log(imgUser.textContent)
+//         //const imgTitle = document.createElement("h2")
+//     }) no muestra los tres gifs tendencia
+// }
